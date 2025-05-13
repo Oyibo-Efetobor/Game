c30df0386662ce6de787007fdf7cd37d8c32193b
OYIBO Efetobor George <efetobor3x@gmail.com>
2025-05-13 11:34:29 +0100

    refactored code

--- TIC TAC TOE GAME BY EFETOBOR.py
+++ TIC TAC TOE GAME BY EFETOBOR.py
@@ -1,124 +1,90 @@
-#JESUS IS LORD!
-#TIC TAC TOE GAME BY ME!!
+# JESUS IS LORD!
+# TIC TAC TOE GAME BY ME!!
 # PLEASE FOLLOW ON INSTAGRAM @tobor._
-def displayboard(): #prints sample board
-    gb= [' ','1','2','3','4','5','6','7','8','9']
-    print(gb[1],gb[2] ,gb[3], sep=" | ")
-    print('---------')
-    print(gb[4],gb[5] ,gb[6], sep=" | ")
-    print('---------')
-    print(gb[7],gb[8] ,gb[9], sep=" | ")
-def displayboard_2(gb1): #prints actual game board for playing
-    print('\n'*3)
-    print(gb1[1] , "|" , gb1[2] , '|' ,  gb1[3])
-    print('---------')
-    print(gb1[4] , "|" , gb1[5] , "|" , gb1[6])
-    print('---------')
-    print(gb1[7] , "|" , gb1[8] , "|" , gb1[9])
+
+def display_sample_board():
+    """Prints a sample board with positions."""
+    sample_board = [' ', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    print_board(sample_board)
+
+def print_board(board):
+    """Prints the current game board."""
+    print('\n' * 3)
+    print(f"{board[1]} | {board[2]} | {board[3]}")
+    print("---------")
+    print(f"{board[4]} | {board[5]} | {board[6]}")
+    print("---------")
+    print(f"{board[7]} | {board[8]} | {board[9]}")
 
 def player_input():
-    position = 0
-    while position not in range(1,10):
-         position = int(input('please input a position (1-9): '))
-    return position
-
-
-def play(position,board,marker):
+    """Gets a valid position input from the player."""
     while True:
-        if board[position] == ' ': #checks to make sure you don't play in a filled up position
-            board[position] = marker
-            break
-        else:
-            print(" ", sep="\n\n")
-            print("try another space!!!")
-            print()
-            position = int(input('please input a position (1-9): '))
-counter2 = 0
-tries = 0
-gb1= [' ']*10
-displayboard()
-displayboard_2(gb1)
-while True:
-    position = player_input()
+        try:
+            position = int(input("Please input a position (1-9): "))
+            if position in range(1, 10):
+                return position
+            else:
+                print("Invalid input. Please choose a number between 1 and 9.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
-    play(position,gb1,'X')
-    tries += 1 #counts for each time you have a valid play
-    if tries > 8:
-        counter2 += 1
-        displayboard_2(gb1)
-        break
-    print()
+def play(position, board, marker):
+    """Places the player's marker on the board."""
+    while board[position] != ' ':
+        print("This position is already taken. Try another space!")
+        position = player_input()
+    board[position] = marker
 
-    displayboard_2(gb1)
-    # conditions under check if you win, it has a total of 8 combinations  
-    if ' ' not in gb1[1] and gb1[2] and gb1[3]:
-        if gb1[1] == gb1[2] and gb1[2] == gb1[3]:
-            break
-    if ' ' not in gb1[4] and gb1[5] and gb1[6]:
-        if gb1[4] == gb1[5] and gb1[5] == gb1[6]:
-            break
-    if ' ' not in gb1[7] and gb1[8] and gb1[9]:
-        if gb1[7] == gb1[8] and gb1[8] == gb1[9]:
-            break
-    if ' ' not in gb1[1] and gb1[4] and gb1[7]:
-        if gb1[1] == gb1[4] and gb1[4] == gb1[7]:
-            break
-    if ' ' not in gb1[2] and gb1[5] and gb1[8]:
-        if gb1[2] == gb1[5] and gb1[5] == gb1[8]:
-            break
-    if ' ' not in gb1[3] and gb1[6] and gb1[9]:
-        if gb1[3] == gb1[6] and gb1[6] == gb1[9]:
-            break
-    if ' ' not in gb1[1] and gb1[5] and gb1[9]:
-        if gb1[1] == gb1[5] and gb1[5] == gb1[9]:
-            break
-    if ' ' not in gb1[3] and gb1[5] and gb1[7]:
-        if gb1[3] == gb1[5] and gb1[5] == gb1[7]:
-            break
-    position = player_input()
+def check_winner(board, marker):
+    """Checks if the given marker has won."""
+    win_combinations = [
+        [1, 2, 3], [4, 5, 6], [7, 8, 9],  # Rows
+        [1, 4, 7], [2, 5, 8], [3, 6, 9],  # Columns
+        [1, 5, 9], [3, 5, 7]              # Diagonals
+    ]
+    return any(all(board[pos] == marker for pos in combo) for combo in win_combinations)
 
-    play(position,gb1,'O')
-    tries += 1
-    if tries > 8:
-        counter2 += 1
-        displayboard_2(gb1)
-        break
-    print()
+def is_draw(board):
+    """Checks if the game is a draw."""
+    return ' ' not in board[1:]
 
-    displayboard_2(gb1)
+def tic_tac_toe():
+    """Main function to run the Tic Tac Toe game."""
+    print("Welcome to Tic Tac Toe!")
+    display_sample_board()
 
-    if ' ' not in gb1[1] and gb1[2] and gb1[3]:
-        if gb1[1] == gb1[2] and gb1[2] == gb1[3]:
+    board = [' '] * 10
+    print_board(board)
+
+    tries = 0
+    while True:
+        # Player X's turn
+        print("Player X's turn:")
+        position = player_input()
+        play(position, board, 'X')
+        tries += 1
+        print_board(board)
+
+        if check_winner(board, 'X'):
+            print("X wins!!!!!!!!!")
             break
-    if ' ' not in gb1[4] and gb1[5] and gb1[6]:
-        if gb1[4] == gb1[5] and gb1[5] == gb1[6]:
+        if tries == 9:
+            print("It's a draw!")
             break
-    if ' ' not in gb1[7] and gb1[8] and gb1[9]:
-        if gb1[7] == gb1[8] and gb1[8] == gb1[9]:
+
+        # Player O's turn
+        print("Player O's turn:")
+        position = player_input()
+        play(position, board, 'O')
+        tries += 1
+        print_board(board)
+
+        if check_winner(board, 'O'):
+            print("O wins!!!!!!!!!")
             break
-    if ' ' not in gb1[1] and gb1[4] and gb1[7]:
-        if gb1[1] == gb1[4] and gb1[4] == gb1[7]:
+        if tries == 9:
+            print("It's a draw!")
             break
-    if ' ' not in gb1[2] and gb1[5] and gb1[8]:
-        if gb1[2] == gb1[5] and gb1[5] == gb1[8]:
-            break
-    if ' ' not in gb1[3] and gb1[6] and gb1[9]:
-        if gb1[3] == gb1[6] and gb1[6] == gb1[9]:
-            break
-    if ' ' not in gb1[1] and gb1[5] and gb1[9]:
-        if gb1[1] == gb1[5] and gb1[5] == gb1[9]:
-            break
-    if ' ' not in gb1[3] and gb1[5] and gb1[7]:
-        if gb1[3] == gb1[5] and gb1[5] == gb1[7]:
-            break
-if counter2 == 1: # this happens if the board is full and no win condition was satisfied
-    print()
-    print("It's a draw")
-else: # this happens if any of the 8 win conditions were satisfied
-    if gb1[1] == gb1[2] and gb1[2] == gb1[3] == 'X' or gb1[4] == gb1[5] and gb1[5] == gb1[6] == 'X' or gb1[7] == gb1[8] and gb1[8] == gb1[9] =='X' or gb1[1] == gb1[4] and gb1[4] == gb1[7] == 'X' or gb1[2] == gb1[5] and gb1[5] == gb1[8] == 'X' or gb1[3] == gb1[6] and gb1[6] == gb1[9] == 'X' or gb1[1] == gb1[5] and gb1[5] == gb1[9] == 'X' or gb1[3] == gb1[5] and gb1[5] == gb1[7] == 'X':
-        print()
-        print("X wins!!!!!!!!!")
-    else:
-        print()
-        print("O wins!!!!!!!!!")
-# This is a  game I applied with my basic knowlegde of lists and functions
+
+# Run the game
+tic_tac_toe()
